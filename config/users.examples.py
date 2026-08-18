17df8462659426426834a8a835228468fa3ec882
Iryna Nikiforova <irina.nkf@gmail.com>
2026-08-18 13:33:37 -0400

    API for users_me, users, Auth, Products

---
+++ config/users.examples.py
@@ -0,0 +1,13 @@
+USER1 = {
+    "email": "your_test_user_email",
+    "password": "your_test_user_password",
+    "role": "user",
+    "name": "Jane Doe"
+}
+
+ADMIN = {
+    "email": "your_admin_email",
+    "password": "your_admin_password",
+    "role": "admin",
+    "name": "John Doe"
+}
